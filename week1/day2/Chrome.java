c383bec6bc639d2a23c5277c49d6e4aeaae31553
MohammedMadeena07 <smlmansoor01@gmail.com>
2025-09-07 14:43:58 +0530

    Add files via upload
    Week1-Day2-DailyAssignment

---
+++ week1/day2/Chrome.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class Chrome {
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+
+		System.out.println("Browser Version: " + version);
+		System.out.println("Browser Developer: " + developer);
+		System.out.println("Is Browser Beta: " + isBeta);
+		System.out.println("Browser Release Year: " + releaseYear);
+		System.out.println("Browser Shortcut Key: " + shortcutKey);
+	}
+}
---
+++ week1/day2/ClassNumberIsPositive.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class ClassNumberIsPositive {
+	public static void main(String[] args) {
+		int version = 10;
+		if (version > 0) {
+			System.out.println("Number is Positive");
+		} else if (version < 0) {
+			System.out.println("Number is Negative");
+		} else {
+			System.out.println("Number is 0");
+		}
+	}
+}
