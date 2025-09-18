cb643a5bee5bca4b5c60437a5247ec87d359016d
MohammedMadeena07 <smlmansoor01@gmail.com>
2025-09-18 17:39:55 +0530

    Add files via upload
    Updated

--- week3/day2/overloading/APIClient.java
+++ week3/day2/overloading/APIClient.java
@@ -2,12 +2,12 @@ package week3.day2.overloading;
 
 public class APIClient {
 	public void sendRequest(String endpoint) {
-		System.out.println("*****Method with One Attribute*****");
+		System.out.println("*****Same Method with One Attribute*****");
 		System.out.println("Request End Point: " + endpoint);
 	}
 
 	public void sendRequest(String endpoint, String requestBody, boolean requestStatus) {
-		System.out.println("*****Method with Three Attributes*****");
+		System.out.println("*****Same Method with Three Attributes*****");
 		System.out.println("Request Body: " + requestBody);
 		System.out.println("Request End point: " + endpoint);
 		System.out.println("Request Status: " + requestStatus);
