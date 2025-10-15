3386a1ec1579613ed62a5da7b3c9b81bad4e174f
MohammedMadeena07 <smlmansoor01@gmail.com>
2025-10-15 18:52:13 +0530

    Update FibonacciSeries.java
    corrected

--- week2/day1/FibonacciSeries.java
+++ week2/day1/FibonacciSeries.java
@@ -4,13 +4,14 @@ public class FibonacciSeries {
 	public static void main(String[] args) {
 		int range = 8;
 		int a = 0, b = 1;
-		int fib;
+		int c;
 		System.out.println("Fibonacci Series of range " + range + " is:");
 		for (int i = 1; i <= range; i++) {
 			System.out.println(a);
-			fib = a + b;
+			c = a + b;
 			a = b;
-			b = fib;
+			b = c;
 		}
 	}
 }
+
